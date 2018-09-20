4021d1f7c7101cac72597eaecd6cddcbf565b293
mkw98 <40563214+mkw98@users.noreply.github.com>
2018-09-20 10:03:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,14 @@ var Counter = React.createClass({
         };
     },
 
+    increment: function() {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    },
+
     render: function() {
-        return React.createElement('div', {},
+        return React.createElement('div', {onClick: this.increment},
             React.createElement('span', {}, 'Licznik ' + this.state.counter)
         );
     }
