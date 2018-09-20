b2159d7276923dabefb11764eab107f9c164cfe8
mkw98 <40563214+mkw98@users.noreply.github.com>
2018-09-20 10:18:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,12 @@ var Counter = React.createClass({
             counter: this.state.counter + 1
         });
     },
+    
+        decrement: function() {
+        this.setState({
+            counter: this.state.counter - 1
+        });
+    },
 
     render: function() {
         return React.createElement('div', {onClick: this.increment},
