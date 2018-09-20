216914bb830c2739d7cd469b0adf15249c15744c
mkw98 <40563214+mkw98@users.noreply.github.com>
2018-09-20 10:02:16 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+var Counter = React.createClass({
+    getInitialState: function() {
+        return {
+            counter: 0
+        };
+    },
+
+    render: function() {
+        return React.createElement('div', {},
+            React.createElement('span', {}, 'Licznik ' + this.state.counter)
+        );
+    }
+});
+
+var element = React.createElement(Counter);
+ReactDOM.render(element, document.getElementById('app'));
