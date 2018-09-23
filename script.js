0622d5f2d39ddaea6bd70800f5c0cd97e6c8881e
mkw98 <40563214+mkw98@users.noreply.github.com>
2018-09-23 11:45:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,10 +17,42 @@ var Counter = React.createClass({
         });
     },
 
-//componentWillReceiveProps
-//shouldComponentUpdate
-//componentWillUpdate
-//componentDidUpdate
+//	getDefaultProps: function() {
+//    	console.log('Faza inicjalizacji. pomysl na uzycie? ')
+//    },
+	
+//	componentWillMount : function() {
+//    	console.log('Faza inicjalizacji. metoda wywoływane tylko w momencie inicjalizacji komponentu.  wywoływana zaraz przed wykonaniem metody render. 
+	//Ustawienie stanu w tej metodzie nie spowoduje przerenderowania komponentu. pomysl na uzycie? ')
+//    },  
+	
+//	componentDidMount: function() {
+//    	console.log('Faza inicjalizacji. metoda wywoływane tylko w momencie inicjalizacji komponentu. wywoływana PO wywoalniu metody render.Pomyslna uzycieie? ')
+//    },  
+//	
+//	componentWillReceiveProps: function() {
+//    	console.log('Faza aktualizacji.  Metoda zostanie wywołana tylko wtedy, gdy komponent otrzyma nowe właściwości i nie jest to faza pierwszego renderowania (montowanie komponentu). Metoda ta pozwala aktualizować stan na podstawie nadchodzących właściwości.  góry", czyli od rodzica komponentu.pomysl na uzycie ')
+//    },
+//	shouldComponentUpdate: function() {
+//    	console.log('Faza aktualizacji. Metoda ta jest wywoływana tuż PRZED wywołaniem metody render i pozwala sprawdzić, 
+	//czy faktycznie trzeba 
+	//jeszcze raz przerysować komponent. Zwracana jest tutaj wartość true/false, czyli typ boolean. 
+	//Metodę tę możemy zastosować, kiedy bardzo zależy nam na optymalizacji naszej aplikacji pomysl na uzycie? ')
+//    },
+//	componentWillUpdate: function() {
+//    	console.log('Faza aktualizacji. Wywoływana, jeśli metoda shouldComponentUpdate zwróci wartość true. Powinna zostać wywoływana tylko do przygotowania na nadchodzące zmiany. 
+	//Nie wolno w tym miejscu modyfikować stanu (metoda setState tutaj nie działa).pomysl na uzycie ')
+//    },
+//
+// 	componentDidUpdate: function() {
+//    	console.log('Faza aktualizacji. wywołuje się jako ostatnia po przerysowaniu komponentu.
+	//w niej możemy wykonać np. jakieś manipulacje DOM (analogicznie do metody componentDidMount).pomysl na uzycie ')
+//    },
+//	componentWillUnmount: function() {
+//	console.log( faza usuwania. 
+//	możemy wykonywać wszystkie rzeczy związane z odpinaniem timerów czy nasłuchiwania zdarzeń na elementach DOM. Pomyslna uzycie?
+//    };
+
 	
     render: function() {
         return React.createElement('div', {},
