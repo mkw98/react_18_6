40cfb85b783c1c152650921a48c1170178a17c05
mkw98 <40563214+mkw98@users.noreply.github.com>
2018-09-24 13:19:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,43 +17,36 @@ var Counter = React.createClass({
         });
     },
 
-//	getDefaultProps: function() {
-//    	console.log('Metoda wywoływana w fazie inicjalizacji. Ustawia domyślne wartości propsów.')
-//    },
+	getDefaultProps: function() {
+    	console.log('Metoda wywoływana w fazie inicjalizacji. Ustawia domyślne wartości propsów.')
+    };
 	
-//	componentWillMount : function() {
-//    	console.log('Metoda, która działa, ale wg dokumentacji już się od niej odchodzi. Metoda wywoływana tylko w momencie inicjalizacji komponentu.  Wywoływana zaraz przed wykonaniem metody render. 
-	//Ustawienie stanu w tej metodzie nie spowoduje przerenderowania komponentu. ')
-//    },  
+	componentWillMount : function() {
+    	console.log('Metoda, która działa, ale wg dokumentacji już się od niej odchodzi. Metoda wywoływana tylko w momencie inicjalizacji komponentu.  Wywoływana zaraz przed wykonaniem metody render. 
+	Ustawienie stanu w tej metodzie nie spowoduje przerenderowania komponentu. ')
+    };  
 	
-//	componentDidMount: function() {
-//    	console.log('Faza inicjalizacji. Metoda wywoływane tylko w momencie inicjalizacji komponentu. 
-//	wywoływana PO wywolaniu metody render. Może służyć do wczytania danych z serwera')
-//    },  
-//	
-//	componentWillReceiveProps: function() {
-//    	console.log('Faza aktualizacji.  Metoda zostanie wywołana tylko wtedy, gdy komponent otrzyma nowe właściwości
-//	i nie jest to faza pierwszego renderowania (montowanie komponentu). Metoda ta pozwala aktualizować stan na podstawie nadchodzących właściwości.  góry", czyli od rodzica komponentu.pomysl na uzycie ')
-//    },
-//	shouldComponentUpdate: function() {
-//    	console.log('Faza aktualizacji. Metoda ta jest wywoływana tuż PRZED wywołaniem metody render i pozwala sprawdzić, 
-	//czy faktycznie trzeba 
-	//jeszcze raz przerysować komponent. Zwracana jest tutaj wartość true/false, czyli typ boolean. 
-	//Metodę tę możemy zastosować, kiedy bardzo zależy nam na optymalizacji naszej aplikacji pomysl na uzycie? ')
-//    },
-//	componentWillUpdate: function() {
-//    	console.log('Faza aktualizacji. Wywoływana, jeśli metoda shouldComponentUpdate zwróci wartość true. Powinna zostać wywoływana tylko do przygotowania na nadchodzące zmiany. 
-	//Nie wolno w tym miejscu modyfikować stanu (metoda setState tutaj nie działa).pomysl na uzycie ')
-//    },
-//
-// 	componentDidUpdate: function() {
-//    	console.log('Faza aktualizacji. wywołuje się jako ostatnia po przerysowaniu komponentu.
-	//w niej możemy wykonać np. jakieś manipulacje DOM (analogicznie do metody componentDidMount).pomysl na uzycie ')
-//    },
-//	componentWillUnmount: function() {
-//	console.log( faza usuwania. 
-//	możemy wykonywać wszystkie rzeczy związane z odpinaniem timerów czy nasłuchiwania zdarzeń na elementach DOM. Pomyslna uzycie?
-//    };
+	componentDidMount: function() {
+    	console.log('Faza inicjalizacji. Metoda wywoływana tylko w momencie inicjalizacji komponentu. Wywoływana PO wywolaniu metody render. Może służyć do wczytania danych z serwera')
+    };  
+	
+	componentWillReceiveProps: function() {
+    	console.log('Faza aktualizacji. Metoda, która działa, ale wg dokumentacji już się od niej odchodzi. Metoda zostanie wywołana tylko wtedy, gdy komponent otrzyma nowe właściwości i nie jest to faza pierwszego renderowania (montowanie komponentu). Metoda pozwala aktualizować stan na podstawie nadchodzących właściwości.  ')
+    };
+	shouldComponentUpdate: function() {
+    	console.log('Faza aktualizacji. Metoda ta jest wywoływana tuż PRZED wywołaniem metody render i pozwala sprawdzić, czy faktycznie trzeba jeszcze raz przerysować komponent. Zwracana jest tutaj wartość true/false. 
+	Metodę tę możemy zastosować, kiedy bardzo zależy nam na optymalizacji aplikacji ')
+    };
+	componentWillUpdate: function() {
+    	console.log('Faza aktualizacji. Metoda, która działa, ale wg dokumentacji już się od niej odchodzi. Wywoływana, jeśli metoda shouldComponentUpdate zwróci wartość true. Powinna zostać wywoływana tylko do przygotowania na nadchodzące zmiany. nie wolno w tym miejscu modyfikować stanu (metoda setState nie działa). ')
+    };
+
+ 	componentDidUpdate: function() {
+    	console.log('Faza aktualizacji. wywołuje się jako ostatnia po przerysowaniu komponentu. możemy wykonać np.  manipulacje DOM (analogicznie do metody componentDidMount). Można sprawdzić czy wywołanie zaplanowanych efektów ubocznych jest potrzebne.  ')
+    };
+	componentWillUnmount: function() {
+	console.log( 'faza usuwania. możemy wykonywać wszystkie rzeczy związane z odpinaniem liczników czy odłączaniem event listenerów'
+   };
 
 	
     render: function() {
